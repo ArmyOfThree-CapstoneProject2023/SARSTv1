803a37cff213e7c875e28fc64fd2ee82219e3751
Lawrence Counts Jr <lcounts@ualr.edu>
2023-04-18 15:42:45 -0500

    First Commit

---
+++ backendprac/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//mongoose connects Node.js/Express.js with the cloud mongodb SARSTdb
+mongoose.connect('mongodb+srv://Lcounts:haptm140AxW25kyE@sarstdb.afmestn.mongodb.net/?retryWrites=true&w=majority')
+//this then func notifies the connection was successful
+.then(() => {
+  console.log('Connection to database is established')
+})
+//this 'catch' func notifies if the connection was not successful
+.catch(() => {
+  console.log('Connection to database is not established')
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  reduce.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Hello from larry!!');
+});
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
